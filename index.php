50849ee9d04c4e5f0e044b7ff730e6c4d7486851
Kay Khahima <42249295+kaykhahima@users.noreply.github.com>
2020-03-28 14:39:13 +0300

    responsive in large screens

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="assets/css/custom.css" />
     <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
     <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.2.0/dist/simpleParallax.min.js"></script>
     <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
