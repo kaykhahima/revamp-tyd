2efe5953e7cc3d044a81c3f6b037ec299697a335
Kay Khahima <42249295+kaykhahima@users.noreply.github.com>
2020-04-09 21:22:03 +0300

    embedded twitter now visible on the home page

--- news/all.php
+++ news/all.php
@@ -70,16 +70,6 @@
                             </div>
                         </div>
                     </div>
-                    <div class="span-1-75">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col">
-                                    <a class="twitter-timeline" data-theme="light" data-height="450" href="https://twitter.com/IncubatorTyd?ref_src=twsrc%5Etfw">Tweets by IncubatorTyd</a>
-                                    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
 
             </section>
